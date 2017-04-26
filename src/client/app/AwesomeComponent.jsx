fc6eb4ea3f77907168d831be5888b2ff3a9d1b49
Ricardo Valencia <ricardovalquin@gmail.com>
2017-04-26 18:16:08 -0500

    resolving a promise

--- src/client/app/AwesomeComponent.jsx
+++ src/client/app/AwesomeComponent.jsx
@@ -4,18 +4,24 @@ class AwesomeComponent extends React.Component {
 
   constructor(props) {
     super(props);
-    this.state = {likesCount : 0};
+    this.state = {likesCount : 0,
+    variable: 'Hello from react'};
     this.onLike = this.onLike.bind(this);
   }
 
   onLike () {
     let newLikesCount = this.state.likesCount + 1;
+    Promise.resolve('resolving a promise').then(
+      (x) => this.setState({variable: x}),
+      (error) => this.setState({variable: 'NO'})
+    );
     this.setState({likesCount: newLikesCount});
   }
 
   render() {
     return (
       <div>
+        <span>{this.state.variable}</span> <br/>
         Likes : <span>{this.state.likesCount}</span>
         <div><button onClick={this.onLike}>Like Me</button></div>
       </div>
