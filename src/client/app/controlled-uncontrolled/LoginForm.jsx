ebc86313c0c7a81db96083e6e8bfcf1182fbb263
Mateo Restrepo <maresres@hotmail.com>
2017-05-07 21:22:56 -0500

    Adding new controlled vs uncontrolled example

---
+++ src/client/app/controlled-uncontrolled/LoginForm.jsx
@@ -0,0 +1,41 @@
+import React from 'react';
+import ControlledPassword from './ControlledPassword.jsx';
+
+class LoginForm extends React.Component {
+
+  constructor(props){
+    super(props);
+    this.props = props;
+    this.state = {
+      email: '',
+      password: ''
+    };
+    this.handlePasswordChange = this.handlePasswordChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handlePasswordChange(value) {
+    this.setState({password: value});
+  }
+
+  handleSubmit(e) {
+  	this.setState({email: this.email});
+  	console.log(this.state);
+  }
+
+  render() {
+    return (
+      <form onSubmit={this.handleSubmit}>
+      	<label>
+      		email
+        	<input type="email" ref={(input) => this.email = input} />
+        </label>
+        <ControlledPassword password={this.state.password} minimum="7" onPasswordChange={this.handlePasswordChange}/>
+        <input type="submit" value="Submit" />
+      </div>
+    );
+  }
+
+}
+
+export default LoginForm;
